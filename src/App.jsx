0e5b316ed3feac0ea1e2a99c599e2dc8aaa5d74f
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2024-02-29 16:06:10 -0300

    feat: Fazendo e Estilizando a Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Navbar } from "./components"
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App bg-[#F1F1F1]">
+      <Navbar />
       <h1 className='font-bold'>Breaking News</h1>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Logo from "../assets/logo-breaking-news.png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <nav className="flex w-full justify-between items-center p-4 fixed top-0 z-10 bg-white shadow-sm">
+        <div className="relative w-[220px] flex items-center">
+          <i className="bi bi-search absolute top-[1] right-1 bg-[#f5f5f5] text-[#757575] rounded-[0.3rem] p-2 z-10"></i>
+          <input type="text" placeholder="Pesquise por um título" className="outline-none p-[0.6rem] bg-[#f5f5f5] border-none w-full rounded-[0.3rem] focus:border-[#0bade3] focus:border-[1px] focus:border-solid" />
+        </div>
+
+        <img src={Logo} alt="Logo" className="w-[8rem] h-[3.5rem] object-cover cursor-pointer self-center" />
+
+        <button className="text-white bg-[#0bade3] border-none outline-none text-[1rem] rounded-md font-medium py-2 px-4 uppercase ease-in-out duration-[0.4s] hover:bg-[#0a86af]">
+          Entrar
+        </button>
+      </nav>
+    </>
   )
 }
 
