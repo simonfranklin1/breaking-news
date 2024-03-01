f6805463852920926a4ef1b0046a8f304a33227c
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2024-03-01 18:26:51 -0300

    feat: Implementando função de busca de notícias

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,12 +10,12 @@ const Card = ({ title, text, img, likes, comments, top = false }) => {
 
   return (
     <div className={`flex flex-col shadow-md rounded-md bg-white overflow-hidden duration-300 hover:shadow-xl cursor-pointer hover:scale-105 w-full ${top? "" : "h-[220px]"}`}>
-      <div className="flex justify-between w-full h-full gap-4">
+      <div className="flex justify-between w-full h-full">
         
         <article className="flex flex-col justify-between p-4">
           <div className="flex flex-col w-full">
             <h2 className={`mb-4 font-bold ${top ? "lg:text-[3rem] lg:leading-[3rem] text-lg" : "lg:text-[1.5rem] text-lg"}`}>{title}</h2>
-            <p className={`text-justify ${top ? "lg:text-[1.5rem]" : "text-[.9rem]"}`}>{limitText(text, 150)}</p>
+            <p className={`text-justify ${top ? "lg:text-[1.5rem]" : "text-[.9rem]"}`}>{limitText(text, 80)}</p>
           </div>
           <div className="flex items-center gap-4">
             <div className='flex items-center gap-[.2rem]'>
@@ -30,6 +30,7 @@ const Card = ({ title, text, img, likes, comments, top = false }) => {
         </article>
 
         <img className='w-[40%] lg:object-cover object-contain object-center' src={img} alt={img} />
+      
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import Logo from "../assets/logo-breaking-news.png"
 import CustomButton from "./CustomButton"
+import { useNavigate } from "react-router-dom"
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    if(search.length) {
+      navigate("/search/" + search) 
+    } else {
+      return;
+    }
+  }
+
   return (
     <>
       <nav className="flex w-full justify-between items-center p-4 fixed top-0 z-10 bg-white shadow-md">
-        <div className="relative w-[220px] flex items-center">
+        <form className="relative w-[220px] flex items-center" onSubmit={handleSearch}>
           <i className="bi bi-search absolute top-[1] right-1 bg-[#f5f5f5] text-[#757575] rounded-[0.3rem] p-2 z-10"></i>
-          <input type="text" placeholder="Pesquise por um título" className="outline-none p-[0.6rem] bg-[#f5f5f5] border-none w-full rounded-[0.3rem] focus:border-[#0bade3] focus:border-[1px] focus:border-solid" />
-        </div>
+          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Pesquise por um título" className="outline-none p-[0.6rem] bg-[#f5f5f5] border-none w-full rounded-[0.3rem] focus:border-[#0bade3] focus:border-[1px] focus:border-solid" />
+        </form>
 
         <img src={Logo} alt="Logo" className="w-[8rem] h-[3.5rem] object-cover cursor-pointer self-center" />
 
