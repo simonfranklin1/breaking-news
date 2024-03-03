f40e3fd710f5614291ba23a8cd250a1ef714e18e
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2024-03-03 16:02:47 -0300

    feat: Fazendo layout dos botões de editar e deletar post

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -2,34 +2,35 @@ import { Link } from 'react-router-dom';
 
 const PostContent = ({ post, comments, likes }) => {
   return (
-    <div className={`flex flex-col cursor-pointer w-full min-h-[400px] mb-5`}>
-            <div className="flex justify-between w-full h-full flex-1 gap-5">
-              <article className="flex flex-col justify-between p-5 gap-5">
-                <div className="flex flex-col w-full">
-                  <h2 className={`mb-4 font-bold lg:text-[3rem] lg:leading-[3rem]`}>{post.title}</h2>
-                  <p className={`text-justify lg:text-[1.5rem]`}>{post.text}</p>
-                </div>
-                <div className='flex flex-col gap-1'>
-                  <Link to={"/profile/" + post.creator.id}>
-                    <div className="text-[#2C8AB4]">
-                      by @{post.creator.username}
-                    </div>
-                  </Link>
-                  <div className="flex items-center gap-4">
-                    <div className='flex items-center gap-[.2rem]'>
-                      <i className="bi bi-hand-thumbs-up"></i>
-                      <span>{likes.length}</span>
-                    </div>
-                    <div className='flex items-center gap-[.2rem]'>
-                      <i className="bi bi-chat"></i>
-                      <span>{comments.length}</span>
-                    </div>
-                  </div>
-                </div>
-              </article>
-              <img className='max-w-[40%] flex-1 object-cover object-center' src={post.banner} alt={post.title} />
+    <div className={`flex flex-col w-full min-h-[400px]`}>
+      <div className="flex justify-between w-full h-full flex-1 gap-5 p-5">
+        <article className="flex flex-col justify-between gap-5">
+          <div className="flex flex-col w-full">
+            <h2 className={`mb-4 font-bold lg:text-[2.5rem] lg:leading-[3rem]`}>{post.title}</h2>
+            <p className={`text-justify lg:text-[1.1rem]`}>{post.text}</p>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <div className="text-[#2C8AB4] w-auto">
+              <Link to={"/profile/" + post.creator.id}>
+                by @{post.creator.username}
+              </Link>
+            </div>
+            <div className="flex items-center gap-4">
+              <div className='flex items-center gap-[.2rem]'>
+                <i className="bi bi-hand-thumbs-up"></i>
+                <span>{likes.length}</span>
+              </div>
+              <div className='flex items-center gap-[.2rem]'>
+                <i className="bi bi-chat"></i>
+                <span>{comments.length}</span>
+              </div>
             </div>
           </div>
+        </article>
+        <img className='max-w-[50%] flex-1 object-cover rounded-md' src={post.banner} alt={post.title} />
+      </div>
+    </div>
   )
 }
 
--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -12,13 +12,34 @@ const NewsPage = () => {
   const [post, setPost] = useState(null);
   const [comment, setComment] = useState("");
 
-  const [comments, setComments] = useState([]);
-  const [likes, setLikes] = useState([]);
+  const [comments, setComments] = useState(null);
+  const [likes, setLikes] = useState(null);
   const [alreadyLiked, setAlreadyLiked] = useState(false);
 
   const { id } = useParams();
   const isLogged = getLocalStorage("access_token");
   const navigate = useNavigate();
+
+  const [openMenu, setOpenMenu] = useState(false);
+
+  useEffect(() => {
+    if (isLogged && isLogged.token) {
+      getPostById(id, isLogged.token).then((response) => {
+        setPost(response.news);
+        setComments(response.news.comments);
+        setLikes(response.news.likes);
+
+        const liked = response.news.likes.find((like) => like.userId == isLogged.id);
+
+        if (liked) {
+          setAlreadyLiked(true);
+        }
+      })
+    } else {
+      navigate("/");
+    }
+
+  }, [id])
 
   const handleComment = (e) => {
     e.preventDefault();
@@ -31,42 +52,35 @@ const NewsPage = () => {
 
   const handleLike = async () => {
     likePost(post.id, isLogged.token).then(response => {
-        if(response.message === "Liked post successfully.") {
-          setLikes([...likes, { userId: user.id , createdAt: new Date()}]);
-          setAlreadyLiked(true);
-        } else {
-          const removeLike = likes.filter((like) => like.userId !== user.id)
-          setLikes(removeLike);
-          setAlreadyLiked(false);
-        }
+      if (response.message === "Liked post successfully.") {
+        setLikes([...likes, { userId: user.id, createdAt: new Date() }]);
+        setAlreadyLiked(true);
+      } else {
+        const removeLike = likes.filter((like) => like.userId !== user.id)
+        setLikes(removeLike);
+        setAlreadyLiked(false);
+      }
     })
   }
-
-  useEffect(() => {
-    if (isLogged && isLogged.token) {
-      getPostById(id, isLogged.token).then((response) => {
-        setPost(response.news);
-        setComments(response.news.comments);
-        setLikes(response.news.likes);
-      })
-
-      const liked = likes.find((like) => like.userId == user.id);
-
-      if(liked) {
-        setAlreadyLiked(true);
-      }
-
-    } else {
-      navigate("/");
-    }
-
-  }, [id])
 
 
   return (
     <>
       {post && (
-        <div className='w-full flex flex-col bg-white rounded-md  overflow-hidden'>
+        <div className='w-full flex flex-col bg-white rounded-lg overflow-hidden relative group'>
+          <button className={`absolute top-1 right-1 bg-[#2C8AB4] flex items-center justify-center w-[2.5rem] h-[2.5rem] rounded-full text-white text-xl group-hover:opacity-100 opacity-0 duration-200 ${isLogged.id === post.creator.id ? "block" : "hidden"}`} onClick={() => setOpenMenu(prev => !prev)}>
+            <div className='relative'>
+              <i className="bi bi-three-dots-vertical"></i>
+              <div className={`absolute bg-white overflow-hidden ${ openMenu ? "flex opacity-100" : "hidden opacity-0"} duration-[.4s] ease-in-out rounded-md items-start flex-col shadow-xl top-[40px] text-base right-0 text-black`}>
+                  <div className='flex items-end gap-2 hover:bg-slate-100 duration-200 pb-2 pt-4 px-8 w-full'>
+                  <i className="bi bi-pencil-square"></i> Editar
+                  </div>
+                  <div className='flex items-end gap-2 hover:bg-slate-100 duration-200 pt-2 pb-4 px-8 w-full'>
+                  <i className="bi bi-trash3"></i> Deletar
+                  </div>
+                </div>
+            </div>
+          </button>
           <PostContent post={post} comments={comments} likes={likes} />
           <div className="flex flex-col p-5">
             <PostInteractions comment={comment} setComment={setComment} handleComment={handleComment} username={user.username} likes={likes} handleLike={handleLike} alreadyLiked={alreadyLiked} />
